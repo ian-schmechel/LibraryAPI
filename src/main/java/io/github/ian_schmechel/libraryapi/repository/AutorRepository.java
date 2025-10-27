e8a7e5aff8535c3b8d606136207ff2f5ffa9b9f1
Ian Schmechel <ian.schmechelads@gmail.com>
2025-10-27 12:10:45 -0300

    Criada a interface AutorRepository

---
+++ src/main/java/io/github/ian_schmechel/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,11 @@
+package io.github.ian_schmechel.libraryapi.repository;
+
+import io.github.ian_schmechel.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
