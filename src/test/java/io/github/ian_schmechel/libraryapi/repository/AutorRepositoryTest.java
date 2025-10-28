e454aa5891e373be75b544b669f9e74c599508c9
Ian Schmechel <ian.schmechelads@gmail.com>
2025-10-27 23:31:18 -0300

    Criado métodos de testes como salvar, deletar e atualizar por ID

--- src/test/java/io/github/ian_schmechel/libraryapi/repository/AutorRepositoryTest.java
+++ src/test/java/io/github/ian_schmechel/libraryapi/repository/AutorRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -42,5 +43,29 @@ public class AutorRepositoryTest {
 
             repository.save(autorEncontrado);
         }
+    }
+
+    @Test
+    public void listarTest(){
+        List<Autor> list = repository.findAll();
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void countTest(){
+        System.out.println("Contagem de autores: " + repository.count());
+    }
+
+    @Test
+    public void deletePorIdTest(){
+        var id = UUID.fromString("874de12e-5270-471d-9bc9-4a24de908c36");
+        repository.deleteById(id);
+    }
+
+    @Test
+    public void deleteTest(){
+        var id = UUID.fromString("874de12e-5270-471d-9bc9-4a24de908c36");
+        var maria = repository.findById(id).get();
+        repository.delete(maria);
     }
 }
