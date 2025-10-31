5986a1ccf310333577160e8fabc595a6b5fcfd32
Ian Schmechel <ian.schmechelads@gmail.com>
2025-10-31 12:48:08 -0300

    Criado métodos Test para atualizar e deletar um livro

--- src/test/java/io/github/ian_schmechel/libraryapi/repository/LivroRepositoryTest.java
+++ src/test/java/io/github/ian_schmechel/libraryapi/repository/LivroRepositoryTest.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 class LivroRepositoryTest {
 
     @Autowired
-    LivroRepository livroRepository;
+    LivroRepository repository;
 
     @Autowired
     AutorRepository autorRepository;
@@ -35,7 +35,44 @@ class LivroRepositoryTest {
 
         livro.setAutor(autor);
 
-        livroRepository.save(livro);
+        repository.save(livro);
     }
 
+    @Test
+    void salvarCascadeTest(){
+        Livro livro = new Livro();
+        livro.setIsbn("90887-84874");
+        livro.setPreco(BigDecimal.valueOf(100));
+        livro.setGenero(GeneroLivro.FICCAO);
+        livro.setTitulo("UFO");
+        livro.setDataPublicacao(LocalDate.of(1980, 1, 2));
+
+        Autor autor = new Autor();
+        autor.setName("João");
+        autor.setNacionalidade("Brasileira");
+        autor.setDataNascimento(LocalDate.of(1951, 1, 31));
+
+        livro.setAutor(autor);
+
+        repository.save(livro);
+    }
+
+    @Test
+    void atualizarAutorLivro(){
+        UUID id = UUID.fromString("55cc3d6d-7e6e-413a-8db9-1445e2bf82d3");
+        var livroParaAtualizar = repository.findById(id).orElse(null);
+
+        UUID idAutor = UUID.fromString("874de12e-5270-471d-9bc9-4a24de908c36");
+        Autor maria = autorRepository.findById(idAutor).orElse(null);
+
+        livroParaAtualizar.setAutor(maria);
+
+        repository.save(livroParaAtualizar);
+    }
+
+    @Test
+    void deletar(){
+        UUID id = UUID.fromString("55cc3d6d-7e6e-413a-8db9-1445e2bf82d3");
+        repository.deleteById(id);
+    }
 }
